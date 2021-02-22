42ac084aa36af9bc55113e4eeeb2fc4c4fe6da96
belbanas <belbanas@gmail.com>
2021-02-22 12:55:34 +0100

    add model for images

---
+++ app/Models/Image.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Models;
+
+
+use CodeIgniter\Model;
+
+class Image extends Model
+{
+    protected $table      = 'images';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = false;
+
+    protected $returnType     = 'object';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = ['name', 'author'];
+
+    protected $useTimestamps = true;
+    protected $createdField  = '';
+    protected $updatedField  = '';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
