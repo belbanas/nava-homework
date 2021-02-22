21d9513092aedf89f806949590516004ab10176e
belbanas <belbanas@gmail.com>
2021-02-22 12:18:02 +0100

    add sql dump file

---
+++ db_dump.sql
@@ -0,0 +1,7 @@
+INSERT INTO images (name, author)
+VALUES
+       ('Járműipar - Ikarus autóbusz', 'Gere László'),
+       ('Járműipar Háborús bűnösök - Szálasi Ferenc', 'Reismann Mariann'),
+       ('Légi közlekedés - Húsz éves a Ferihegyi repülőtér', 'Bara István'),
+       ('Vállalat - Autóbuszmodellek készülnek a Fővárosi Autóbuszüzemben', 'Pálfai Gábor'),
+       ('Közlekedés - Budapest - A MÁV modern személyvonata', 'Faludi Imre');
