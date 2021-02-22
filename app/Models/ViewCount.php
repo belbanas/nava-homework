fd6bdc2e8416dfa579d5362047f2c78527e79df6
belbanas <belbanas@gmail.com>
2021-02-22 16:41:08 +0100

    add viewcount model for query results

---
+++ app/Models/ViewCount.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Models;
+
+
+use CodeIgniter\Model;
+
+class ViewCount extends Model
+{
+    protected $table      = 'views';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'object';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = ['image_id', 'view_count'];
+
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
