d8c1dbfba6bbcbf92e217753b18884bcf5078af1
belbanas <belbanas@gmail.com>
2021-02-22 21:01:36 +0100

    add exception handling for image controller

--- app/Controllers/ImageController.php
+++ app/Controllers/ImageController.php
@@ -25,35 +25,51 @@ class ImageController extends BaseController
 
     public function create()
     {
-        $data = [
-            "name" => $this->request->getVar('name'),
-            "author" => $this->request->getVar('author'),
-        ];
-        $this->imageModel->insert($data);
-        return $this->response->setJSON([
-            "message" => "Record added successfully"
-        ])->setStatusCode(201);
+        try {
+            $data = [
+                "name" => $this->request->getVar('name'),
+                "author" => $this->request->getVar('author'),
+            ];
+            $this->imageModel->insert($data);
+            return $this->response->setJSON([
+                "message" => "Record added successfully"
+            ])->setStatusCode(201);
+        } catch (\Exception $e) {
+            return $this->response->setJSON([
+                "Error" => $e->getMessage()
+            ]);
+        }
     }
 
     public function update($id)
     {
-        $name = $this->request->getVar('name');
-        $author = $this->request->getVar('author');
-        $data = [
-            "name" => $name,
-            "author" => $author,
-        ];
-        $this->imageModel->update($id, $data);
-        return $this->response->setJSON([
-            "message" => "Record updated successfully"
-        ])->setStatusCode(200);
+        try {
+            $data = [
+                "name" => $this->request->getVar('name'),
+                "author" => $this->request->getVar('author'),
+            ];
+            $this->imageModel->update($id, $data);
+            return $this->response->setJSON([
+                "message" => "Record updated successfully"
+            ])->setStatusCode(200);
+        } catch (\Exception $e) {
+            return $this->response->setJSON([
+                "Error" => $e->getMessage()
+            ]);
+        }
     }
 
     public function delete($id)
     {
-        $this->imageModel->delete($id);
-        return $this->response->setJSON([
-            "message" => "Record deleted successfully"
-        ])->setStatusCode(200);
+        try {
+            $this->imageModel->delete($id);
+            return $this->response->setJSON([
+                "message" => "Record deleted successfully"
+            ])->setStatusCode(200);
+        } catch (\Exception $e) {
+            return $this->response->setJSON([
+                "Error" => $e->getMessage()
+            ]);
+        }
     }
 }
