3ac2c705b078c72d15d5339c3f31bd10c77ec045
belbanas <belbanas@gmail.com>
2021-02-22 12:55:23 +0100

    add unique constraint to sql

--- db_init.sql
+++ db_init.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS views;
 DROP TABLE IF EXISTS images;
 
 CREATE TABLE images (
-                        id BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                        id BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT UNIQUE,
                         name VARCHAR(255) NOT NULL,
                         author VARCHAR(255) NOT NULL,
                         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -11,7 +11,7 @@ CREATE TABLE images (
 );
 
 CREATE TABLE views (
-                       id SERIAL PRIMARY KEY,
+                       id SERIAL PRIMARY KEY UNIQUE,
                        image_id BIGINT UNSIGNED NOT NULL,
                        view_count INT,
                        FOREIGN KEY(image_id) REFERENCES images(id)
