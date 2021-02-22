a69f73b00efab144981e4e26157cc4531e4f4880
belbanas <belbanas@gmail.com>
2021-02-22 12:08:12 +0100

    db init file

---
+++ db_init.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS views;
+DROP TABLE IF EXISTS images;
+
+CREATE TABLE images (
+                        id BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                        name VARCHAR(255) NOT NULL,
+                        author VARCHAR(255) NOT NULL,
+                        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                        updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+                        deleted_at DATETIME
+);
+
+CREATE TABLE views (
+                       id SERIAL PRIMARY KEY,
+                       image_id BIGINT UNSIGNED NOT NULL,
+                       view_count INT,
+                       FOREIGN KEY(image_id) REFERENCES images(id)
+);
