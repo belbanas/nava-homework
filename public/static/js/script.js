fa061e55cba2378bd85b38d5c8ab543cb0340375
belbanas <belbanas@gmail.com>
2021-02-23 08:47:28 +0100

    fix visualization of view counts

--- public/static/js/script.js
+++ public/static/js/script.js
@@ -6,26 +6,30 @@ const DOM = {
         fetch('/images')
             .then(response => response.json())
             .then(json_response => {
-                console.log(json_response)
                 let tableBody = document.querySelector('#table-body');
                 for (let image of json_response.result) {
                     let viewCount = 0;
                     fetch('images/' + image.id + "/count")
                         .then(resp => resp.json())
                         .then(json_resp => {
-                            viewCount = json_resp.message
+                            if (json_resp.view_count) {
+                                viewCount = json_resp.view_count
+                            } else {
+                                console.log(json_resp);
+                            }
+                            let HTML = "";
+                            HTML += `<tr>`
+                            HTML += `<th>${image.id}</th>`
+                            HTML += `<td>${image.author}</td>`
+                            HTML += `<td>${image.name}</td>`
+                            HTML += `<td>${viewCount}</td>`
+                            HTML += `</tr>`
+                            tableBody.innerHTML += HTML;
                         })
-                    let HTML = "";
-                    HTML += `<tr>`
-                    HTML += `<th>${image.id}</th>`
-                    HTML += `<td>${image.author}</td>`
-                    HTML += `<td>${image.name}</td>`
-                    HTML += `<td>${viewCount}</td>`
-                    HTML += `</tr>`
-                    tableBody.innerHTML += HTML;
                 }
             })
     }
 }
 
 DOM.init();
+
