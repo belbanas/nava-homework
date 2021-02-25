41bb66c877127d300447e5628bf0344a40b86db8
belbanas <belbanas@gmail.com>
2021-02-25 18:21:26 +0100

    add route for viewing image

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -38,6 +38,7 @@ $routes->get('/', 'Home::tableView');
 $routes->get('/images', 'ImageController::index');
 $routes->post('/images', 'ImageController::create');
 $routes->put('/images/(:num)', 'ImageController::update/$1');
+$routes->get('/images/(:num)', 'ImageController::view/$1');
 $routes->delete('/images/(:num)', 'ImageController::delete/$1');
 $routes->get('/images/(:num)/count', 'ViewCountController::index/$1');
 $routes->post('/images/(:num)/count', 'ViewCountController::incrementCounter/$1');
